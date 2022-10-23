1b6b99b6ea3d693a5a86935b20ef695c0b13a195
myuon <ioi.joi.koi.loi@gmail.com>
2022-10-23 13:44:02 +0900

    chore: setup web

---
+++ web/.eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "css-reorder"],
+  rules: {
+    "react/self-closing-comp": "error",
+    "css-reorder/property-reorder": "error",
+    "react/display-name": "off",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    // for emotion
+    // cf: https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/no-unknown-property.md#rule-options
+    "react/no-unknown-property": ["error", { ignore: ["css"] }],
+  },
+  ignorePatterns: [".eslintrc.js"],
+};
