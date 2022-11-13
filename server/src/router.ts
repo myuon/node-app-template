fc587dee8ca908a973791615f802e13171534d99
myuon <ioi.joi.koi.loi@gmail.com>
2022-11-13 16:34:19 +0900

    feat: body parsing example

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,10 +1,24 @@
+import koaBody from "koa-body";
 import Router, { IRouterOptions } from "koa-router";
+import { z } from "zod";
 
 export const newRouter = (options?: IRouterOptions) => {
   const router = new Router(options);
 
   router.get("/hello", async (ctx) => {
     ctx.body = "Hello World!";
+  });
+  router.post("/echo", koaBody(), async (ctx) => {
+    const schema = z.object({
+      message: z.string(),
+    });
+    const result = schema.safeParse(ctx.request.body);
+    if (!result.success) {
+      ctx.throw(400, result.error);
+      return;
+    }
+
+    ctx.body = result.data.message;
   });
 
   return router;
